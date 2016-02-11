cef9c674940e69986099b33bdd9832c47491e24d
Jennifer Jackson <jlynne93@hotmail.com>
2016-02-11 07:40:58 -0500

    First commit

---
+++ Section2Lab/Section2Lab/Person.cs
@@ -0,0 +1,35 @@
+﻿namespace Section2Lab
+{
+    class Person
+    {
+
+        public int age;
+        public string firstName;
+        public string lastName;
+        public bool isMarried;
+        public Person spouse;
+
+        public static double sumOfAllAges;
+
+        public string GetFullName()
+        {
+
+            string fullName = "";
+            fullName += this.firstName;
+            fullName += " ";
+            fullName += this.lastName;
+            return fullName;
+
+        }
+
+        public void PrintNameAndAge()
+        {
+
+            System.Console.WriteLine("Full Name: " + this.GetFullName());
+            System.Console.WriteLine("Age: " + this.age);
+
+        }
+
+
+    }
+}
---
+++ Section2Lab/Section2Lab/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Section2Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person p1 = new Person();
+
+            System.Console.Write("What is your first name? ");
+            p1.firstName = System.Console.ReadLine();
+
+            System.Console.Write("What is your last name? ");
+            p1.lastName = System.Console.ReadLine();
+
+            System.Console.Write("What is your age? ");
+            p1.age = int.Parse(System.Console.ReadLine());
+
+            System.Console.Write("Are you married? (Yes/No) ");
+            p1.isMarried = bool.Parse(System.Console.ReadLine());
+
+            System.Console.Write("What is the first name of your spouse? ");
+            p1.spouse = System.Console.ReadLine();
+
+            System.Console.Write("What is the age of your spouse? ");
+            p1.spouse = System.Console.ReadLine();
+
+            //p1.spouse;
+
+            Person p2 = new Person();
+            //p2.spouse;
+
+            System.Console.Write("Press any key to continue...");
+            System.Console.ReadKey();
+        }
+    }
+}
