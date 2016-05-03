13535030589edb8656b33562d0ff8e06d0d0c030
Jennifer Jackson <jlynne93@hotmail.com>
2016-05-03 15:05:07 -0400

    Added comments

--- Section5Lab/Section5Lab/Program.cs
+++ Section5Lab/Section5Lab/Program.cs
@@ -4,22 +4,27 @@
     {
         static void Main(string[] args)
         {
+
+            // Program purpose:
+            // Create course with array of students, amount specified by user
+            // Store student name and number in student class through course object
+            // Allows user to start over if they made a mistake
+
             System.Console.WriteLine("Welcome to the course creator.");
 
             System.Console.Write("Would you like to create a new course? (y/n)  : ");
             string createCourse = System.Console.ReadLine();
             if (createCourse.ToLower().Contains("y"))
             {
-
                 Course course = Course.GetCourseInfo();
 
-                System.Console.Write("Is all the information correct? (y/n) : ");
+                System.Console.Write("Is all the information correct? (y/n)         : ");
                 string correctCourseInfo = System.Console.ReadLine();
                 while (correctCourseInfo.ToLower().Contains("n"))
                 {
                     System.Console.WriteLine("Please reenter course information.");
                     course = Course.GetCourseInfo();
-                    System.Console.Write("Is all the information correct? (y/n) : ");
+                    System.Console.Write("Is all the information correct? (y/n)         : ");
                     correctCourseInfo = System.Console.ReadLine();
                 }
             }
