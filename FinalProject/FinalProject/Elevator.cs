86399dbef90e856123cb706c6d7676041e6aadab
Jennifer Jackson <jlynne93@hotmail.com>
2016-05-10 14:25:04 -0400

    Final Project Completed

---
+++ FinalProject/FinalProject/Elevator.cs
@@ -0,0 +1,43 @@
+﻿namespace FinalProject
+{
+    class Elevator
+    {
+        private double MaxWeight;
+        private Passenger[] Occupants;
+
+        public Elevator(int maxOccupants, double maxWeight)
+        {
+            Occupants = new Passenger[maxOccupants];
+            this.MaxWeight = maxWeight;
+        }
+
+        public void AddOccupant(Passenger passenger, int index)
+        {
+            Occupants[index] = passenger;
+        }
+
+        public double GetCurrentWeight()
+        {
+            double CurrentWeight = 0;
+            for (int i = 0; i < Occupants.Length; i++)
+            {
+                CurrentWeight = CurrentWeight + Occupants[i].GetWeight();
+            }
+            return CurrentWeight;
+        }
+
+        public bool IsOverMaxCapacity()
+        {
+            bool OverMaxCapacity = true;
+            if (GetCurrentWeight() > MaxWeight)
+            {
+                OverMaxCapacity = true;
+            }
+            else
+            {
+                OverMaxCapacity = false;
+            }
+            return OverMaxCapacity;
+        }
+    }
+}
